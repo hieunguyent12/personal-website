eb27db46514abef6c9504e221484eb95fce0177d
Hieu <hieunguyen.0527@gmail.com>
2023-09-09 23:21:13 -0400

    changed intro, added darkmode and more

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      screens: {
+        sm: "100%",
+        md: "730px",
+        lg: "730px",
+        xl: "730px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
